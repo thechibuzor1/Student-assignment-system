5d6074feb008231be40d325a92e79eaff92a1d06
Chibuzor <chibuzorm26@gmail.com>
2025-03-29 09:36:41 +0100

    footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
   </main>
   <footer class="bg-gray-200 text-center p-4 mt-8">
-    <p>Group 3 Project Assignment System</p>
+    <p>Group 3 Project</p>
   </footer>
   <script src="/public/js/app.js"></script>
 </body>
